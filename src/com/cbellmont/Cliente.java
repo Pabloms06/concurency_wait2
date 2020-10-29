5daabb7a6e3a6c557fc34125bf9c544b1ceaffee
pablo <pablomarcossanchez06.pm@gmail.com>
2020-10-29 14:14:42 +0100

    Cambiado, entrega 2 (Callback)

--- src/com/cbellmont/Cliente.java
+++ src/com/cbellmont/Cliente.java
@@ -2,26 +2,30 @@ package com.cbellmont;
 
 public class Cliente extends Thread {
 
+    Camarero camarero;
+
+    public Cliente (Camarero camarero){
+        this.camarero=camarero;
+    }
+
+
 
     @Override
     public void run() {
         try {
             while (true) {
-                if (Main.list.isEmpty()) {
-                    System.out.println("Cliente - Voy a esperar a que me traigan comida...");
+                    if (Main.list.isEmpty()){
+                        camarero.prepararComanda();
+                        System.out.println("Cliente - Voy a esperar a que me traigan comida...");
 
-                } else {
-                    System.out.println("Cliente - Voy a consumir un " + Main.list.get(0) + " 3 segundos");
-                    Main.list.remove(0);
-                    sleep(3000);
+                    } else {
+                        System.out.println("Cliente - Voy a consumir un " + Main.list.get(0) + " 3 segundos");
+                        Main.list.remove(0);
+                        sleep(3000);
+                    }
                 }
-            }
-        } catch (
-                InterruptedException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
     }
-
 }
-
